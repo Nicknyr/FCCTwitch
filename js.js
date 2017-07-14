459666c36f0af6285b82908db2d9bcff8c01a9e1
Nick Kinlen <nick_kinlen@hotmail.com>
2017-07-14 16:36:37 -0400

    Offline channels working

--- js.js
+++ js.js
@@ -12,30 +12,32 @@ $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?'
     }
 });
 
+
 for(var i = 0; i < streamers.length; i++){
-    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + streamers[i] +'?callback=?').done(function(data2) {
+    $.getJSON('https://wind-bow.glitch.me/twitch-api/streams/' + streamers[i]).done(function(data2) {
         //console.log(data2);
 
-
         if(data2.stream != null){
-          $('#channelsOnline').prepend("<img src=" + data2.stream.channel.logo + ">" + "<h3><a href=" + data2.stream.channel.url + "</h3><ul><li>" + data2.stream.channel.display_name + " is online now <a/></li><li>followers: " + data2.stream.channel.followers + "</li><li> viewers: " + data2.stream.viewers + "</li><li> game: " + data2.stream.channel.game + "</li>")
+          $('#channelsOnline').prepend("<img src=" + data2.stream.channel.logo + ">" + "<h3><a href=" + data2.stream.channel.url + "></h3><ul><li>" + data2.stream.channel.display_name + " is online now <a/></li><li>followers: " + data2.stream.channel.followers + "</li><li> viewers: " + data2.stream.viewers + "</li><li> game: " + data2.stream.channel.game + "</li>")
           }
-        else {
-          // Separates name of channel from the rest of the URL
+          /* Separates name of channel from the rest of the URL
           var channelUrl = data2._links.channel;
           var channelName = channelUrl.match(/([^\/]*)\/*$/)[1];
+          */
+  });
+};
 
-          $('#channelsOffline').prepend("<a href=" + data2._links.channel + "><h3>" + channelName + " is offline" + "</h3>")
+for(var j = 0; j < streamers.length; j++) {
+  $.getJSON('https://wind-bow.glitch.me/twitch-api/channels/'+streamers[j]).done(function(data3){
+    console.log(data3);
 
-        }
+    if(data3.error !== true){
+    $('#channelsOffline').prepend("<img src=" + data3.logo + ">" + "<h3><a href=" + data3.url + "></h3><ul><li>" + data3.name + " is offline </a></li><li>follower: " + data3.followers + "</li><li> views: " + data3.views + "</li>")
+    }
 
-    });
-
-  };
-
-for(var i = 0; i < streamers.length; i++){
-  $.getJSON('https://wind-bow.gomix.me/twitch-api/users?login=' + streamers[i] + 'callback=?').done(function(data3){
-      console.log(data3);
+    else {
+      $('#channelsOffline').prepend("<h3>Channel does not exist</h3>")
+    }
   });
 };
 
@@ -53,7 +55,5 @@ for(var i = 0; i < streamers.length; i++){
     $('#channelsOnline').show();
     $('#channelsOffline').show();
   });
-
-
 
 });
